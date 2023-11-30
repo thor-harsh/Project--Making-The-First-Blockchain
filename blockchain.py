7154f8fe760151e169f34376630812f9357d86f3
thor-harsh <105957576+thor-harsh@users.noreply.github.com>
2023-11-30 23:13:34 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,3 @@
-# Module 1 - Create a Blockchain
-
-# To be installed:
-# Flask==0.12.2: pip install Flask==0.12.2
-# Postman HTTP Client: https://www.getpostman.com/
-
 # Importing the libraries
 import datetime
 import hashlib
